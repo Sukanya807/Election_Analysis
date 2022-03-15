6148478fcf021dd4750a696628232097b34877dc
Sukanya Ghosh <sukanyaghosh807@gmail.com>
2022-03-15 16:19:41 -0400

    adding the election audit code

--- pypoll.py
+++ pypoll.py
@@ -5,32 +5,65 @@
 # The winner of the election based on the popular vote
 
 #Assign a variable for thr file to load and the path
-file_to_load = 'c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/Resources/election_results.csv'
-with open(file_to_load) as election_data:
-    print(election_data)
+# ADD OUR DEPENDENCIES
 import csv
 import os
+#ASSIGN A VARIABLE TO LOAD A FILE FROM A PATH
+file_to_load = 'c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/Resources/election_results.csv'
+#ASSIGN A VARIABLE TO SAVE THE FILE TO A PATH
+file_to_save = os.path.join('c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/analysis/election_analysis.txt')   
+#iNITIALIZE A TOTAL VOTE COUNTER
+total_votes=0
 
-file_to_save = os.path.join('c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/analysis/election_analysis.txt')
-with open(file_to_save,"w") as txt_file:
-   
-    txt_file.write("Counties in the Election\n-----------------------\nArapahoe\nDenver\nJefferson")
-    
-import csv
-import os   
-file_to_load = 'c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/Resources/election_results.csv'
-file_to_save = os.path.join('c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/analysis/election_analysis.txt')   
+#declaring list for the candidate options
+candidate_options=[]
+
+#CREATE A DICTIONARY 
+candidate_votes={}
+#CREATE VARIABLES
+winning_candidate=""
+winning_count=0
+winning_percentage=0
+#OPEN THE ELECTION RESULTS AND READ THE FILE
 with open(file_to_load) as election_data:
     file_reader=csv.reader(election_data)
-    for row in file_reader:
-        print(row)
-import csv
-import os   
-file_to_load = 'c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/Resources/election_results.csv'
-file_to_save = os.path.join('c:/Users/SUKANYA GHOSH/Desktop/Election_Analysis_Practice/analysis/election_analysis.txt')   
-with open(file_to_load) as election_data:
-    file_reader=csv.reader(election_data)
+
+#READ THE HEADER ROW    
     headers=next(file_reader)
-    print(headers)
+
+#PRINT EACH ROW IN THE CSV FILE
+    for row in file_reader:
+        #ADD TO THE TOTAL VOTE COUNT
+        total_votes +=1
+
+#PRINT THE CANDIDATE NAME FROM EACH ROW
+        candidate_name=row[2]
+#IF THE CANDIDATE DOES NOT MATCH ANY EXISTING CANDIDATE
+        if candidate_name not in candidate_options:
+#ADD IT TO THE LIST OF CANDIDATES
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name] +=1
+
+for candidate_name in candidate_votes:
+    votes=candidate_votes[candidate_name]
+    vote_percentage=float(votes)/float(total_votes)*100
+    
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    if(votes>winning_count) and (vote_percentage>winning_percentage):
+        winning_count=votes
+        winning_percentage=vote_percentage
+        winning_candidate=candidate_name
+winning_candidate_summary=(f"---------------------------\n"
+                           f"Winner: {winning_candidate}\n"
+                           f"Winning Vote Count: {winning_count:,}\n"
+                           f"Winning Percentage: {winning_percentage:.1f}%\n"
+                           f"--------------------------\n")
+print(winning_candidate_summary)
+
+
+
+
 
 
